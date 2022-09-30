8b86e6d13a3d6e3a96c01c4b3aa32c75fcfaa94f
ok-open-sc <114725648+ok-open-sc@users.noreply.github.com>
2022-09-30 12:27:32 -0400

    Create Merge_sort.py

---
+++ Sorts/Merge_sort.py
@@ -0,0 +1,32 @@
+# Write a merge sort algorithm
+
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+
+    mid = len(list) // 2
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    left_index = right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+if __name__ == '__main__':
+    user_input = input('Enter numbers separated by a comma:').strip()
+    unsorted = [int(item) for item in user_input.split(',')]
+    print(merge_sort(unsorted))
